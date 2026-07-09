1006d8cb0f95426a4786bc19c70d6355da39cf27
유정 <choiyu0wo0d@tukorea.ac.kr>
2026-07-09 22:35:02 +0900

    style: Point페이지 UI 수정

---
+++ src/components/points/PointList.tsx
@@ -0,0 +1,42 @@
+export const PointList = () => {
+    return (
+        <div className="flex flex-col w-full gap-3.5">
+            <div className="flex justify-between px-5 py-4 rounded-lg bg-gray-50">
+                <div className="flex flex-col gap-1">
+                    <div className="font-bold">코알라 티셔츠</div>
+                    <div className="font-semibold text-sm text-gray-500">
+                        26.06.26
+                    </div>
+                </div>
+                <div className="font-bold text-[#5A60F9]">+ 3,000p</div>
+            </div>
+            <div className="flex justify-between px-5 py-4 rounded-lg bg-gray-50">
+                <div className="flex flex-col gap-1">
+                    <div className="font-bold">프로그래밍 전공책</div>
+                    <div className="font-semibold text-sm text-gray-500">
+                        26.06.20
+                    </div>
+                </div>
+                <div className="font-bold text-[#5A60F9]">+ 1,000p</div>
+            </div>
+            <div className="flex justify-between px-5 py-4 rounded-lg bg-gray-50">
+                <div className="flex flex-col gap-1">
+                    <div className="font-bold">선러브 모자</div>
+                    <div className="font-semibold text-sm text-gray-500">
+                        26.04.06
+                    </div>
+                </div>
+                <div className="font-bold text-[#5A60F9]">+ 2,000p</div>
+            </div>
+            <div className="flex justify-between px-5 py-4 rounded-lg bg-gray-50">
+                <div className="flex flex-col gap-1">
+                    <div className="font-bold">오렌지</div>
+                    <div className="font-semibold text-sm text-gray-500">
+                        26.04.01
+                    </div>
+                </div>
+                <div className="font-bold text-[#5A60F9]">+ 1,000p</div>
+            </div>
+        </div>
+    );
+};
