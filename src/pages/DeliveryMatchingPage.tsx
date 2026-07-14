3ab7caad05758fae6e626b0c1c147f1cf0f5b74b
유정 <choiyu0wo0d@tukorea.ac.kr>
2026-07-14 22:45:45 +0900

    feat: 매칭 페이지에서 수락 버튼 클릭시 추적 페이지로 이동

--- src/pages/DeliveryMatchingPage.tsx
+++ src/pages/DeliveryMatchingPage.tsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import ChevronIcon from "../assets/icons/ChevronIcon";
 import { DeliveryInfo } from "../components/delivery/DeliveryInfo";
 import { DeliveryRoute } from "../components/delivery/DeliveryRoute";
 
 export default function DeliveryMatchingPage() {
+    const navigate = useNavigate();
+    const handleAccept = () => {
+        navigate("/delivery/tracking");
+    };
+
     return (
         <div className="page-container">
             <div className="flex relative items-center justify-center text-gray-500">
@@ -18,11 +24,14 @@ export default function DeliveryMatchingPage() {
                 itemSize="S"
                 settlementPoint="3,200P"
             />
-            <div className="fixed bottom-14 left-1/2 flex w-full max-w-[402px] -translate-x-1/2 gap-3.5 px-5">
+            <div className="fixed bottom-10 left-1/2 flex w-full max-w-[402px] -translate-x-1/2 gap-3.5 px-5">
                 <button className="flex flex-1 items-center justify-center bg-gray-100 text-gray-600 font-bold rounded-lg px-2.5 py-3.5">
                     거절하기
                 </button>
-                <button className="flex flex-1 items-center justify-center bg-purple-500 text-white font-bold rounded-lg px-2.5 py-3.5">
+                <button
+                    onClick={handleAccept}
+                    className="flex flex-1 items-center justify-center bg-purple-500 text-white font-bold rounded-lg px-2.5 py-3.5"
+                >
                     수락하기
                 </button>
             </div>
