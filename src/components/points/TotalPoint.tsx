9e4f5f292fe3ca4e0edde203d4e0de377b16204f
유정 <choiyu0wo0d@tukorea.ac.kr>
2026-07-20 15:52:20 +0900

    feat: 포인트 페이지 필터링 버튼 작동

--- src/components/points/TotalPoint.tsx
+++ src/components/points/TotalPoint.tsx
@@ -1,11 +1,16 @@
-export const TotalPoint = () => {
+interface TotalPointProps {
+    total: number;
+}
+export const TotalPoint = ({ total }: TotalPointProps) => {
     return (
         <div className="flex flex-col mt-9 w-full rounded-xl px-6 py-5 gap-5 bg-gradient-to-r from-[#636DFF] to-[#4541EB]">
             <div className="flex flex-col gap-1.5">
                 <div className="font-semibold text-sm text-purple-200">
                     보유 포인트
                 </div>
-                <div className="font-bold text-2xl text-white">7,000p</div>
+                <div className="font-bold text-2xl text-white">
+                    {total.toLocaleString()}P
+                </div>
             </div>
             <div className="flex items-center justify-center w-full px-2.5 py-2 rounded-lg bg-purple-100 text-purple-700 font-semibold">
                 + 충전하기
--- src/types/point.ts
+++ src/types/point.ts
@@ -6,3 +6,31 @@
  * - 자동 정산 결과를 화면과 API에서 함께 사용할 수 있게 합니다.
  * - MVP에서는 자동 정산 중심으로, 출금은 확장 타입으로 남겨둡니다.
  */
+
+export const POINT_FILTER = {
+    ALL: {
+        label: "전체",
+        code: null, // 또는 "ALL" (서버 정책에 맞게)
+    },
+    SAVING: {
+        label: "적립",
+        code: "SAVING",
+    },
+    USE: {
+        label: "사용",
+        code: "USE",
+    },
+    EXPIRE: {
+        label: "소멸",
+        code: "EXPIRE",
+    },
+} as const;
+
+export type PointFilterKey = keyof typeof POINT_FILTER;
+
+export type PointFilterLabel = (typeof POINT_FILTER)[PointFilterKey]["label"];
+
+export type PointFilter = Exclude<
+    (typeof POINT_FILTER)[PointFilterKey]["code"],
+    null
+>;
